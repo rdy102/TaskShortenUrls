6ef091ff1cfe0c47d5926491bcc91edaf1047a57
Taras Samkovskyi <taras.samkovskyi@ratesetter.com>
2022-05-27 16:17:38 +0300

    Init

---
+++ src/UrlShortener.WebApplication/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using UrlShortener.WebApplication.Models;
+
+namespace UrlShortener.WebApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public IActionResult TechTaskDetails()
+        {
+            return View("TechTaskDetails");
+        }
+    }
+}
